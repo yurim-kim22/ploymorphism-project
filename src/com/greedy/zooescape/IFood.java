6bc83de69580671c4089a704fa68df8729412f95
yurim-kim <rionqkd520@gmail.com>
2023-01-04 16:01:18 +0900

    interface commit

---
+++ src/com/greedy/zooescape/IFood.java
@@ -0,0 +1,8 @@
+package com.greedy.zooescape;
+
+public interface IFood {
+	public abstract void iBanana();
+	public abstract void iApple();
+	public abstract void iWatermolon();
+	public abstract void iFoodillness();
+}
